f76bb7e450b1c80c4faad3e080c61a89a5c50326
Tanisha Srivastava <tanisha681216@gmail.com>
2025-01-27 08:03:08 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Footer from "./components/footer";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+function App(){
+  return <div>
+    <Navbar />
+    <Hero />
+    <Footer />
+    </div>
+}
+export default App;
---
+++ Hero.jsx
@@ -0,0 +1,14 @@
+function Hero(){
+    return(
+        <main>
+            <h1>Welcome to learn react</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore atque voluptatum natus aliquam cupiditate expedita sequi mollitia amet placeat deleniti hic ex impedit repudiandae nesciunt ipsam, dicta nostrum temporibus rerum!
+            Esse explicabo deserunt velit tempora tempore laudantium consequatur non iste at amet nulla placeat, consectetur asperiores enim quo? Unde saepe nostrum, ipsum qui dicta eum aut ad cumque obcaecati asperiores!
+            </p>
+            <img src="https://miro.medium.com/v2/resize:fit:1400/0*y6IcBe5J1AdALzXw.png" alt="img" />
+        </main>
+    )
+    
+    
+}
+export default Hero
